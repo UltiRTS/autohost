e86d9f97b7bb873481f5eeba11e8f03a72c7e647
Teresa <teresa@eterea.uk>
2021-07-31 20:00:47 -0400

    uptodate autohost

--- lib/server.py
+++ lib/server.py
@@ -22,56 +22,56 @@ class AutohostServer(threading.Thread):
 
     def autohostInterfaceSayChat(self, msg):
         self.serverNetwork.send(msg)
+       
 
+    def run(self):
+        while True:
 
-def run(self):
-    while True:
+            self.serverNetwork.receive()
+            while self.serverNetwork.hasCmd():
+                #print('autohost server interface triggered')
 
-        self.serverNetwork.receive()
-        while self.serverNetwork.hasCmd():
-            #print('autohost server interface triggered')
+                receivedMsg = self.serverNetwork.nextCmd()
+                #print('AUTOHOST SERVER!!!!!!!!!!!!!:'+str(self.serverNetwork.nextCmd()))
+                if 'No clients connected, shutting down server' in receivedMsg:
+                    ctl = {
+                        "bid": self.bid,
+                        "msg": 'exit',
+                        "caller": self.hostedby,
+                        "ttl": 0, "action": 'exit'}
+                    self.deliver.put(ctl)
 
-            receivedMsg = self.serverNetwork.nextCmd()
-            #print('AUTOHOST SERVER!!!!!!!!!!!!!:'+str(self.serverNetwork.nextCmd()))
-            if 'No clients connected, shutting down server' in receivedMsg:
-                ctl = {
-                    "bid": self.bid,
-                    "msg": 'exit',
-                    "caller": self.hostedby,
-                    "ttl": 0, "action": 'exit'}
-                self.deliver.put(ctl)
+                if receivedMsg[0:3] == b'\r\x00\xfe':
+                    print("1")
 
-            if receivedMsg[0:3] == b'\r\x00\xfe':
-                print("1")
-
-            if re.match(chatMsgPatt, receivedMsg):
-                #receivedMsg = receivedMsg[12:-1]
-                #print(colored('[INFO]', 'cyan'), "received: ", receivedMsg)
-                print(
-                    colored(
-                        '[INFO]',
-                        'green'),
-                    colored(
-                        self.username +
-                        " forwarding to lobby: " +
-                        receivedMsg,
-                        'white'))
-                ctl = {
-                    "bid": self.bid,
-                    "msg": receivedMsg,
-                    "caller": self.hostedby,
-                    "ttl": 0,
-                    "action": 'sayBtlRoom'
-                }
-                self.deliver.put(ctl)
-            else:
-                #print(colored('[INFO]', 'cyan'), "received: ", receivedMsg)
-                print(
-                    colored(
-                        '[INFO]',
-                        'green'),
-                    colored(
-                        self.username +
-                        " received: " +
-                        receivedMsg,
-                        'white'))
+                if re.match(chatMsgPatt, receivedMsg):
+                    #receivedMsg = receivedMsg[12:-1]
+                    #print(colored('[INFO]', 'cyan'), "received: ", receivedMsg)
+                    print(
+                        colored(
+                            '[INFO]',
+                            'green'),
+                        colored(
+                            self.username +
+                            " forwarding to lobby: " +
+                            receivedMsg,
+                            'white'))
+                    ctl = {
+                        "bid": self.bid,
+                        "msg": receivedMsg,
+                        "caller": self.hostedby,
+                        "ttl": 0,
+                        "action": 'sayBtlRoom'
+                    }
+                    self.deliver.put(ctl)
+                else:
+                    #print(colored('[INFO]', 'cyan'), "received: ", receivedMsg)
+                    print(
+                        colored(
+                            '[INFO]',
+                            'green'),
+                        colored(
+                            self.username +
+                            " received: " +
+                            receivedMsg,
+                            'white'))
